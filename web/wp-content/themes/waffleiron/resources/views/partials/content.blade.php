668f8eb8305c56af21e0d5987085d2f23ceb66b9
cwahlfeldt <chriswahlfeldt@gmail.com>
2019-07-12 15:19:27 -0500

    do[e

--- web/wp-content/themes/waffleiron/resources/views/partials/content.blade.php
+++
@@ -1,8 +0,0 @@
-<article @php post_class() @endphp>
-  <header>
-    @include('partials/entry-meta')
-  </header>
-  <div class="entry-summary">
-    @php the_excerpt() @endphp
-  </div>
-</article>
