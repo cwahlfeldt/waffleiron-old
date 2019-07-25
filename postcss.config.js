33169d11ddb56e340268ae11071f81fa65e472dd
cwahlfeldt <chriswahlfeldt@gmail.com>
2019-07-25 13:17:21 -0500

    tm env

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,9 @@
-module.exports = function (arg) {
-  return {
-    plugins: [
-      require('postcss-import')({}),
-      require('postcss-preset-env')({}),
-      require('tailwindcss')({}),
-      require('cssnano')({}),
-    ]
-  }
-}
+module.exports = (ctx) => ({
+  map: true,
+  plugins: [
+    require('postcss-import')({}),
+    require('postcss-preset-env')({}),
+    require('tailwindcss')({}),
+    require('cssnano')({}),
+  ]
+})
