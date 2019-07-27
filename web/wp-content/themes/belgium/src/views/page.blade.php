f89e60383ff487ba0de16d34c20eef750146906f
cwahlfeldt <chriswahlfeldt@gmail.com>
2019-07-27 18:27:32 -0500

    synced

--- web/wp-content/themes/belgium/src/views/page.blade.php
+++ web/wp-content/themes/belgium/src/views/page.blade.php
@@ -1,7 +1,6 @@
 @extends('master')
 
 @section('content')
-<<<<<<< HEAD
   <header class="primary-navigation">
     <nav class="primary">
       <a href="/">
@@ -10,7 +9,4 @@
       {{ json_encode(get_field('primary_nav', 'options')) }}
     </nav>
   </header>
-=======
-  content
->>>>>>> 6331141c40600d0cd3a89e93a2c5b07827a1f76e
 @endsection
