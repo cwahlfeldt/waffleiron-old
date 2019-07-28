3cf6747af18c0f51e12d19d68cc070520f3e2d80
cwahlfeldt <chriswahlfeldt@gmail.com>
2019-07-27 20:13:55 -0500

    synced

--- web/wp-content/themes/belgium/functions.php
+++ web/wp-content/themes/belgium/functions.php
@@ -130,8 +130,8 @@ echo $scripts;
 
 // enqueue scripts/styles
 add_action( 'wp_enqueue_scripts', function() {
-  wp_enqueue_script('waffleiron-script', realpath(__DIR__ . '/../../../public/mod.js'));
-  wp_enqueue_style('waffleiron-style', realpath(__DIR__ . '/../../../public/mod.css'));
+  wp_enqueue_script('waffleiron-script', get_template_directory_uri() . '/public/mod.js'));
+  wp_enqueue_style('waffleiron-style', get_template_directory_uri() . '/public/mod.css'));
 });
 
 // enqueue admin styles
