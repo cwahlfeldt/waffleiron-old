d007226688c38ae1423fb884f33b0cdcdc5ecb2f
cwahlfeldt <chriswahlfeldt@gmail.com>
2019-07-27 19:25:26 -0500

    synced

--- web/wp-content/themes/belgium/functions.php
+++ web/wp-content/themes/belgium/functions.php
@@ -125,13 +125,12 @@ add_action( 'widgets_init', 'waffleiron_widgets_init' );
  */
 //ini_set('xdebug.max_nesting_level', 9999);
 
-$scripts = realpath(__DIR__ . '/../../../public/mod.out.js');
+$scripts = realpath(__DIR__ . '/../../public');
 echo $scripts;
-
 // enqueue scripts/styles
 add_action( 'wp_enqueue_scripts', function() {
-  wp_enqueue_script('waffleiron-script', realpath(__DIR__ . '/../../../public/mod.js'));
-  wp_enqueue_style('waffleiron-style', realpath(__DIR__ . '/../../../public/mod.css'));
+  wp_enqueue_script('waffleiron-script', realpath(__DIR__ . '/../../public/mod.js'));
+  wp_enqueue_style('waffleiron-style', realpath(__DIR__ . '/../../public/mod.css'));
 });
 
 // enqueue admin styles
--- web/wp-content/themes/belgium/src/views/page.blade.php
+++ web/wp-content/themes/belgium/src/views/page.blade.php
@@ -1,16 +1,12 @@
 @extends('master')
 
 @section('content')
-<<<<<<< HEAD
   <header class="primary-navigation">
     <nav class="primary">
       <a href="/">
         <img class="brand"></img>
       </a>
-      {{ json_encode(get_field('primary_nav', 'options')) }}
+      {{ json_encode(get_field('primary_navigation', 'options')) }}
     </nav>
   </header>
-=======
-  content
->>>>>>> 6331141c40600d0cd3a89e93a2c5b07827a1f76e
 @endsection
