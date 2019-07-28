058e1673799a0413b07a9db2d3e4560dfc32f66b
cwahlfeldt <chriswahlfeldt@gmail.com>
2019-07-27 20:01:47 -0500

    synced

--- web/wp-content/themes/belgium/functions.php
+++ web/wp-content/themes/belgium/functions.php
@@ -125,12 +125,13 @@ add_action( 'widgets_init', 'waffleiron_widgets_init' );
  */
 //ini_set('xdebug.max_nesting_level', 9999);
 
-$scripts = realpath(get_template_directory_uri() . '/../../public');
+$scripts = realpath(get_template_directory_uri() . '/../../../public');
 echo $scripts;
+
 // enqueue scripts/styles
 add_action( 'wp_enqueue_scripts', function() {
-  wp_enqueue_script('waffleiron-script', realpath(get_template_directory_uri() . '/../../../public/mod.js'));
-  wp_enqueue_style('waffleiron-style', realpath(get_template_directory_uri() . '/../../../public/mod.css'));
+  wp_enqueue_script('waffleiron-script', realpath(__DIR__ . '/../../../public/mod.js'));
+  wp_enqueue_style('waffleiron-style', realpath(__DIR__ . '/../../../public/mod.css'));
 });
 
 // enqueue admin styles
