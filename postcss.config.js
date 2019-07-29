8b8a6b1e49528de08c736c76059a4204716084bd
cwahlfeldt <chriswahlfeldt@gmail.com>
2019-07-29 05:11:29 -0500

    git workflow issues

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-import')({}),
     require('postcss-preset-env')({}),
-    require('tailwindcss')({}),
+    require('tailwindcss')('web/wp-content/themes/belgium/tailwind.js'),
     require('cssnano')({}),
   ]
 }
