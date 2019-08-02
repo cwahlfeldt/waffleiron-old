eee74b576a69d1ea52b48e5a2f83352de8d8d8b9
cwahlfeldt <chriswahlfeldt@gmail.com>
2019-08-02 14:57:37 -0500

    git workflows

--- web/wp-content/themes/belgium/src/views/single.blade.php
+++ web/wp-content/themes/belgium/src/views/single.blade.php
@@ -1,7 +1,76 @@
 @extends('master')
 
+@php
+  $name = get_the_title();
+  $pic = get_field('picture');
+  $img = get_field('image');
+  $job_title = get_field('title');
+  $accordions = get_field('accordions');
+  $content = get_field('content');
+  $style = get_field('style');
+@endphp
+
 @section('content')
-  <section class="designs">
-    <p>{{ get_the_title() }}</p>
+  <section class="heading w-full relative">
+    <div class="flex sm:flex-row flex-col w-full h-full">
+      <div class="side-wrap h-full sm:order-1 order-2 2xl:w-1/4 md:w-1/2 lg:w-1/3 sm:w-full sm:mr-3 bg-blue relative py-12 px-4" style="background-color: {{ $style['background_color'] }};">
+        <div class="h-full flex flex-col justify-start items-center relative">
+          <img class="logo" src="/wp-content/uploads/2019/07/TM_Icon_Orange.svg" alt="">
+
+          <div class="text my-auto">
+            <hr style="color: {{ $style['line_color'] }};" class="w-10 my-4 border border-orange border-solid border-1"> 
+            <p class="text-white text-center tracking-widest font-condensed uppercase font-thin text-3xl leading-snug">
+              {{ $name }}
+            </p>
+            <p class="text-white text-center tracking-widest font-amp uppercase font-thin text-sm leading-snug">
+              {{ $job_title }}
+            </p>
+            <hr style="color: {{ $style['line_color'] }};" class="w-10 my-5 border border-orange border-solid border-1"> 
+          </div>
+        </div>
+      </div>
+
+      <div class="title sm:order-2 order-1 md:w-1/2 lg:w-3/4 sm:w-full sm:ml-3 md:bg-center bg-left bg-left bg-cover md:px-12 md:py-12 px-8 py-12" style="background-image: url({{ $pic['url'] }});">
+        <div class="flex flex-col justify-end sm:items-end items-center h-full">
+          <p class="heading-branded-text lg:text-left text-center tracking-wider text-white sm:font-lg text-center font-slab font-thin uppercase leading-tight text-5xl">
+            {!! get_field('branding')['title'] !!}
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="heading w-full">
+    <div class="flex sm:flex-row flex-col w-full h-full">
+      <div class="side-wrap h-full sm:order-1 order-2 2xl:w-1/4 md:w-1/2 lg:w-1/3 sm:w-full bg-blue relative py-12 px-4" style="background-color: {{ $style['background_color'] }};">
+        <div class="h-full flex flex-col justify-center items-center relative">
+          <div class="profile md:block hidden flex flex-col items-center justify-center">
+            <div class="bg-white p-4">
+              <img class="" src="{{ $img['url'] }}" alt="">
+              <p class="text-blue w-64 text-left tracking-widest font-slab uppercase font-thin text-lg leading-normal pt-3">
+                {{ $name }}
+              </p>
+              <p class="text-orange w-64 text-left tracking-widest font-amp uppercase font-thin text-sm leading-snug">
+                {{ $job_title }}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="bg-tan py-24 pl-48 pr-32 mt-6 title sm:order-2 order-1 md:w-1/2 lg:w-3/4 sm:w-full sm:ml-3 md:bg-center bg-left bg-left bg-cover">
+        <p class="lg:text-left text-left tracking-wider sm:text-lg text-center font-slab font-thin uppercase leading-tight text-5xl text-orange">
+          Profile
+          {{-- <hr style="border-color: {{ $style['color']['line_color'] }};" class="w-10 my-4 border border-orange border-solid border-1"> --}}
+          <hr style="border-color: ;" class="w-10 my-6 border border-blue mx-0 border-solid border-1">
+          @php
+            global $post;
+            $content = $post->post_content;
+          @endphp
+          <div class="font-serif text-blue py-6">{!! $post->post_content !!}</div>
+
+        </p>
+      </div>
+    </div>
   </section>
 @endsection
