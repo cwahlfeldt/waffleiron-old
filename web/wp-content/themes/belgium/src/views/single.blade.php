6b14c4ad0414f2c471ff342da9f02382218158ed
cwahlfeldt <chriswahlfeldt@gmail.com>
2019-08-02 15:46:44 -0500

    iron work

--- web/wp-content/themes/belgium/src/views/single.blade.php
+++ web/wp-content/themes/belgium/src/views/single.blade.php
@@ -11,7 +11,7 @@
 @endphp
 
 @section('content')
-  <section class="heading w-full relative">
+  <section class="heading attorney-heading w-full relative">
     <div class="flex sm:flex-row flex-col w-full h-full">
       <div class="side-wrap h-full sm:order-1 order-2 2xl:w-1/4 md:w-1/2 lg:w-1/3 sm:w-full sm:mr-3 bg-blue relative py-12 px-4" style="background-color: {{ $style['background_color'] }};">
         <div class="h-full flex flex-col justify-start items-center relative">
@@ -40,25 +40,27 @@
     </div>
   </section>
 
-  <section class="heading w-full">
+  <section class="heading attorney-heading-content h-full w-full">
     <div class="flex sm:flex-row flex-col w-full h-full">
       <div class="side-wrap h-full sm:order-1 order-2 2xl:w-1/4 md:w-1/2 lg:w-1/3 sm:w-full bg-blue relative py-12 px-4" style="background-color: {{ $style['background_color'] }};">
         <div class="h-full flex flex-col justify-center items-center relative">
           <div class="profile md:block hidden flex flex-col items-center justify-center">
-            <div class="bg-white p-4">
+            <div class="bg-white p-6 w-auto">
               <img class="" src="{{ $pic['url'] }}" alt="">
-              <p class="text-blue w-64 text-left tracking-widest font-slab uppercase font-thin text-lg leading-normal pt-3">
-                {{ $name }}
-              </p>
-              <p class="text-orange w-64 text-left tracking-widest font-amp uppercase font-thin text-sm leading-snug">
-                {{ $job_title }}
-              </p>
+              <div class="flex flex-col">
+                <p class="text-blue w-64 text-left tracking-widest font-slab uppercase font-thin text-lg leading-normal pt-3">
+                  {{ $name }}
+                </p>
+                <p class="text-orange w-64 text-left tracking-widest font-amp uppercase font-thin text-sm leading-snug">
+                  {{ $job_title }}
+                </p>             
+              </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div class="bg-tan py-24 pl-48 pr-32 mt-6 title sm:order-2 order-1 md:w-1/2 lg:w-3/4 sm:w-full sm:ml-3 md:bg-center bg-left bg-left bg-cover">
+      <div class="content-thing bg-tan py-24 pl-48 pr-32 mt-6 title sm:order-2 order-1 md:w-1/2 lg:w-3/4 sm:w-full md:bg-center bg-left bg-left bg-cover border-l-8 border-solid border-white">
         <p class="lg:text-left text-left tracking-wider sm:text-lg text-center font-slab font-thin uppercase leading-tight text-5xl text-orange">
           Profile
           {{-- <hr style="border-color: {{ $style['color']['line_color'] }};" class="w-10 my-4 border border-orange border-solid border-1"> --}}
@@ -68,8 +70,20 @@
             $content = $post->post_content;
           @endphp
           <div class="font-serif text-blue py-6">{!! $post->post_content !!}</div>
-
         </p>
+  
+        <div class="accordions w-full h-full">
+
+          <div class="accordion-title border-t-2 border-solid border-blue hover:cursor-point">
+            <h2 class="font-condensed font-bold tracking-wide text-2xl text-blue uppercase hover:text-orange hover:cursor-pointer">
+              Title
+            </h2>
+          </div>
+          <div class="accordion-content pb-3 hidden">
+            Content
+          </div>
+
+        </div>
       </div>
     </div>
   </section>
