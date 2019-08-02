16d6616a5db7b71bd43c50de5fb52ce1d3eef112
cwahlfeldt <chriswahlfeldt@gmail.com>
2019-08-02 12:04:20 -0500

    iron work

--- web/wp-content/themes/belgium/src/views/components/nav.blade.php
+++ web/wp-content/themes/belgium/src/views/components/nav.blade.php
@@ -11,28 +11,13 @@
   $primary_nav = get_field('primary_nav', 'options');
   $home = (array) get_field('primary_nav', 'options')[0]['page']['page'];
   $menu = (array) get_field('primary_nav', 'options');
- // array_shift($menu);
-
-
-  /*
-  $areas_copy = $navigation['primary']['menu'];
-  $attorneys_copy = $navigation['primary']['menu'];
-  $areas_menu = array_slice($areas_copy, 3, 9);
-  $attorneys_menu = array_slice($attorneys_copy, 13, 17);
-  $the_menu = $areas_menu;
-
-  if ($nav->post_name == 'areas-of-practice') {
-    $the_menu = $areas_menu;
-  }
-
-  if ($nav->post_name == 'our-attorneys') {
-    $the_menu = $attorneys_menu;
-  }
-  */
+  array_shift($menu);
+  //echo json_encode($menu);
 @endphp
 
 <header class="primary-navigation container mx-auto h-full">
   <nav class="primary flex md:flex-row flex-col h-full w-full">
+
     <div class="flex flex-row justify-start items-center">
       <a href="/{{ $home['page_name'] }}" class="nav-wrap hover:opacity-75 relative">
         <img class="brand" src="{{ $brand }}" />
@@ -49,11 +34,12 @@
     <section class="primary-menu md:flex md:visible invisible flex-row sm:items-center w-full justify-end ml-auto">
       @foreach ($menu as $nav)
 
-          @if (isset($nav['page']['sub_menu']))
-            {{ 'is set' }}
-            <div id="menu-link-{{ $nav['page']['post_name'] }}" class="{{$nav['page']['post_name']}} md:py-7 menu-link w-auto relative relative top-0 left-0 lg:mx-6 md:mx-2">
+
+          @if (isset($nav['page']['page']))
+            @php $new_nav = $nav['page']['page']; @endphp
+            <div id="menu-link-{{ $new_nav->post_name }}" class="{{$new_nav->post_name}} md:py-7 menu-link w-auto relative relative top-0 left-0 lg:mx-6 md:mx-2">
               <a class="py-7 dib font-wide w-full uppercase font-normal lg:text-sm text-xs text-tan hover:opacity-75 tracking-wide" href="/{{ $nav['page']['post_name'] }}">
-                {{ $nav->post_title }}
+                {{ $nav['page']['label'] }}
               </a>
             </div>
 
@@ -62,15 +48,15 @@
                 <div class="container-sm relative flex flex-col mx-auto justify-between pt-5">
                   <div class="relative h-full">
                     <h2 class="font-sans uppercase text-3xl font-thin border-bottom text-white tracking-widest">
-                      {{ $nav->post_title }}
+                      {{ $nav['page']->post_title }}
                       <hr class="w-10 my-3 border border-tan border-solid border-1 ml-0">
                     </h2>
                     <div class="flex flex-auto flex-wrap items-between w-full py-6">
-                      @foreach($the_menu as $menu)
-                        <div class="lg:w-1/4 md:w-1/3 w-full">
-                          <a class="text-white font-condensed font-medium text-base tracking-wide leading-relaxed" href="/{{ $menu['page']['post_name'] }}/">{{ $menu->post_title }}</a>
-                        </div>
-                      @endforeach
+                      {{-- @foreach($the_menu as $menu) --}}
+                      {{--   <div class="lg:w-1/4 md:w-1/3 w-full"> --}}
+                      {{--     <a class="text-white font-condensed font-medium text-base tracking-wide leading-relaxed" href="/{{ $menu['page']['post_name'] }}/">{{ $menu->post_title }}</a> --}}
+                      {{--   </div> --}}
+                      {{-- @endforeach --}}
                     </div>
                   </div>
                 </div>
