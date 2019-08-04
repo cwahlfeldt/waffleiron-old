4bfe5afb657c0672259f5ea76977b98731f03117
cwahlfeldt <chriswahlfeldt@gmail.com>
2019-08-04 10:29:49 -0500

    iron work

--- web/public/out.js
+++ web/public/out.js
@@ -60,33 +60,67 @@
 //
 // mod.js
 // declare 
-const env = {
-  id : `dope`,
-}
-
 
 (async ({
-  state,
-  actions,
+  $,
 }) => {
 
-  // do stuff here
-  const link = actions.slctr(`.menu-link`)
-  console.log(link)
+
+  // code
+  $('.primary-menu .sub-menu-enabled').hover(function(e) {
+    // console.log(this)
+    let t = e.target
+    console.log('in', t)
+    if (t.classList.contains('dropdown-menu')) {
+      $(t).find('.sub-menu-enabled').addClass('sub-menu-style')
+      return
+    }
+    $(t).addClass('sub-menu-style')
+    // console.log(e.target)
+  }, function(e) {
+    // console.log(e.target)
+    let t = e.target
+    if ( t.classList.contains('sub-menu-enabled') && t.classList.contains('sub-menu-style') && !(t.classList.contains('dropdown-menu')) ) {
+      $('.primary-menu').find('.sub-menu-enabled').removeClass('sub-menu-style')
+    }
+  })
+  // slctr(`.primary-navigation`)
+  //   .addEventListener(`mouseenter`, e => {
+  //     if (e.target.classList.contains(`sub-menu-enabled`) || e.target.classList.contains(`dropdown-menu`)) {
+  //       addBodyClass(`nav-open`);
+  //     }
+  //     if (e.target.classList.contains(`sub-menu-enabled`)) {
+  //       target = e.target
+  //       target.classList.add(`sub-menu-style`)
+  //     }
+  //   }, true)
+
+  // slctr(`.primary-navigation`)
+  //   .addEventListener(`mouseover`, () => {
+  //     if (target.classList.contains(`sub-menu-style`) || target.classList.contains(`dropdown-menu`)) {
+  //       target.classList.remove(`sub-menu-style`)
+  //       removeBodyClass(`nav-open`)
+  //     }
+  //   }, false)
+
+  // slctr(`.primary-navigation .sub-menu-enabled`)
+  //   .addEventListener(`mouseover`, () => {
+  //     toggleBodyClass(`nav-open`)
+  //   }, true)
+  // link.addEventListener(`mouseout`, () => actions.toggleBodyClass(`nav-open`))
+  // dropdown.addEventListener(`mouseenter`, () => actions.toggleBodyClass(`nav-open`))
+  // dropdown.addEventListener(`mouseout`, () => actions.toggleBodyClass(`nav-open`))
+
+  // console.log(state.name)
 
 })({
 
-  // state
-  state             : {
-    name            : `waffles is ${ dope }`,
-    state           : `illinois`,
-  },
-  
-  // actions
-  actions           : {
-    slctr           : s => document.querySelector(`${ s }`),
-    toggleBodyClass : c => document.body.classList.toggle(`${ c }`),
-  },
+  // function defs
+  $ : jQuery,
+  slctr           : s => document.querySelector(`${ s }`),
+  toggleBodyClass : c => document.body.classList.toggle(`${ c }`),
+  addBodyClass : c => document.body.classList.add(`${ c }`),
+  removeBodyClass : c => document.body.classList.remove(`${ c }`),
 
 })
 }()}
--- web/wp-content/themes/belgium/src/views/designs/attorneys.blade.php
+++ web/wp-content/themes/belgium/src/views/designs/attorneys.blade.php
@@ -8,12 +8,12 @@
   <div class="py-16 w-full h-full container-sm mx-auto">
     <h3 class="w-full text-base font-sans tracking-widest text-center">
       {{ $title['title'] }}
-      <hr style="border-color: {{ $style['color']['line_color'] }};" class="w-10 my-4 border border-orange border-solid border-1">
+      <hr style="border-color: {{ $style['color']['line_color'] }};" class="w-10 my-8 border border-orange border-solid border-1">
     </h3>
 
-    <div class="flex flex-auto flex-wrap justify-between w-full">
+    <div class="flex flex-auto flex-wrap justify-between w-full py-12">
       @foreach($attorneys as $attorney)
-        <div class="flex-col flex lg:w-1/5 md:1/4 sm:1/2 h-full mx-3">
+        <div class="flex-col flex md:w-1/5 sm:w-1/3 xs:w-full h-full mx-3">
           <div class="w-full h-64 bg-cover h-64 bg-center bg-gray" style="background-image: url({{ get_field('picture', $attorney->ID)['url'] }});"></div>
           <div class="bg-white w-full h-auto bg-white py-2">
             <h4 class="font-sans text-blue font-bold tracking-wide">
