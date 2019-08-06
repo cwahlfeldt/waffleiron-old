d8c0e21c1ecbb56ffe7438585dc36a5e4d0158aa
cwahlfeldt <chriswahlfeldt@gmail.com>
2019-08-06 12:54:10 -0500

    up and running - pushing to tst after this

--- web/wp-content/themes/belgium/src/scripts/mod.js
+++ web/wp-content/themes/belgium/src/scripts/mod.js
@@ -7,65 +7,94 @@
   }) => {
     $(document).ready(function() {
 
-      let subMenu = false
-      let dropdown = false
+      let $subMenu = false
+      let $dropdown = false
 
-      // code
-      $('.primary-menu .menu-link').mouseover(
-        function() {
-          subMenu = $(this).find('.sub-menu-enabled')
-          dropdown = subMenu.parent().next('.dropdown-menu')
+      $(window).hover(
+        ({ target }) => {
+            if ($(target).is('.sub-menu-enabled')) {
+              console.log( $(target).parent().next() )
 
-          subMenu.toggleClass('sub-menu-style')
-          dropdown.slideDown('fast')
+              $dropdown = $(target).parent().next()
+              $subMenu = $(target)
 
-          // console.log(`mouseover: `, subMenu)
-          // console.log(`mouseover: `, dropdown)
-        }
-      )
-
-      $('.dropdown-menu').hover(
-        function() {
-          // let subMenu = $(this).prev().find('.sub-menu-enabled')
-          // let dropdown = $(this)
-
-          dropdown.css({display: 'flex !important'})
-          dropdown.toggleClass('vis')
-          subMenu.addClass('sub-menu-style')
-
-          // console.log(`dropdown - mouseover: `, subMenu)
-          // console.log(`dropdown - mouseover: `, dropdown)
+              $subMenu.addClass('sub-menu-style')
+              $dropdown.slideDown('fast')
+              return
+            }
+          return
         },
-        function() {
-          // let subMenu = $(this).prev().find('.sub-menu-enabled')
-          // let dropdown = $(this)
+        ({ target }) => {
+          console.log($subMenu)
+          console.log($dropdown)
+          if ($subMenu || $dropdown) {
+            if (!($(target).is('.dropdown-menu *')) && !($(target).is('.dropdown-menu')) && !($(target).is('.sub-menu-enabled'))) {
+              $('.sub-menu-enabled').removeClass('sub-menu-style')
+              $('.dropdown-menu').slideUp('fast')
+              return
+            }
 
-          // dropdown.css('vis')
-          dropdown.css({display: ''})
-          subMenu.removeClass('sub-menu-style')
-          dropdown.toggleClass('vis')
-          // dropdown.slideUp('fast')
-
-          // console.log(`dropdown - mouseout: `, subMenu , '\n')
-          // console.log(`dropdown - mouseout: `, dropdown , '\n')
-        }
-      )
-
-      $('.primary-menu .menu-link').mouseout(
-        function() {
-          // let subMenu = $(this).find('.sub-menu-enabled')
-          // let dropdown = subMenu.parent().next('.dropdown-menu')
-
-          subMenu.removeClass('sub-menu-style')
-
-          // console.log(`mouseout: `, subMenu , '\n')
-          // console.log(`mouseout: `, dropdown , '\n')
-
-          if ( !(dropdown.hasClass('vis')) ) {
-            dropdown.slideUp('fast')
+          return
           }
         }
       )
+
+//       // code
+//       $('.primary-menu .menu-link').mouseover(
+//         function() {
+//           subMenu = $(this).find('.sub-menu-enabled')
+//           dropdown = subMenu.parent().next('.dropdown-menu')
+
+//           subMenu.toggleClass('sub-menu-style')
+//           dropdown.slideDown('fast')
+
+//           // console.log(`mouseover: `, subMenu)
+//           // console.log(`mouseover: `, dropdown)
+//         }
+//       )
+
+//       $('.dropdown-menu').hover(
+//         function() {
+//           // let subMenu = $(this).prev().find('.sub-menu-enabled')
+//           // let dropdown = $(this)
+
+//           dropdown.css({display: 'flex !important'})
+//           dropdown.toggleClass('vis')
+//           subMenu.addClass('sub-menu-style')
+
+//           // console.log(`dropdown - mouseover: `, subMenu)
+//           // console.log(`dropdown - mouseover: `, dropdown)
+//         },
+//         function() {
+//           // let subMenu = $(this).prev().find('.sub-menu-enabled')
+//           // let dropdown = $(this)
+
+//           // dropdown.css('vis')
+//           dropdown.css({display: ''})
+//           subMenu.removeClass('sub-menu-style')
+//           dropdown.toggleClass('vis')
+//           // dropdown.slideUp('fast')
+
+//           // console.log(`dropdown - mouseout: `, subMenu , '\n')
+//           // console.log(`dropdown - mouseout: `, dropdown , '\n')
+//         }
+//       )
+
+//       $('.primary-menu .menu-link').mouseout(
+//         function() {
+//           // let subMenu = $(this).find('.sub-menu-enabled')
+//           // let dropdown = subMenu.parent().next('.dropdown-menu')
+
+//           subMenu.removeClass('sub-menu-style')
+
+//           // console.log(`mouseout: `, subMenu , '\n')
+//           // console.log(`mouseout: `, dropdown , '\n')
+
+//           if ( !(dropdown.hasClass('vis')) ) {
+//             dropdown.slideUp('fast')
+//           }
+//         }
+//       )
 
       $('.carrousel').slick({
         dots: true,
