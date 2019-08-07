54ecfb0f087e025efe5ec80ef555d46e599fa7db
cwahlfeldt <chriswahlfeldt@gmail.com>
2019-08-07 09:09:56 -0500

    cool

--- web/public/out.js
+++ web/public/out.js
@@ -55,154 +55,64 @@
     return require
   }
 
-  Pax.files["/Users/waffles/.dotfiles/Code/Web/waffleiron/web/wp-content/themes/belgium/src/scripts/mod.js"] = file_$2fUsers$2fwaffles$2f$2edotfiles$2fCode$2fWeb$2fwaffleiron$2fweb$2fwp$2dcontent$2fthemes$2fbelgium$2fsrc$2fscripts$2fmod$2ejs; file_$2fUsers$2fwaffles$2f$2edotfiles$2fCode$2fWeb$2fwaffleiron$2fweb$2fwp$2dcontent$2fthemes$2fbelgium$2fsrc$2fscripts$2fmod$2ejs.deps = {}; file_$2fUsers$2fwaffles$2f$2edotfiles$2fCode$2fWeb$2fwaffleiron$2fweb$2fwp$2dcontent$2fthemes$2fbelgium$2fsrc$2fscripts$2fmod$2ejs.filename = "/Users/waffles/.dotfiles/Code/Web/waffleiron/web/wp-content/themes/belgium/src/scripts/mod.js"; function file_$2fUsers$2fwaffles$2f$2edotfiles$2fCode$2fWeb$2fwaffleiron$2fweb$2fwp$2dcontent$2fthemes$2fbelgium$2fsrc$2fscripts$2fmod$2ejs(module, exports, require, __filename, __dirname, __import_meta) {
+  Pax.files["/Users/waffles/Code/Web/waffleiron/web/wp-content/themes/belgium/src/scripts/mod.js"] = file_$2fUsers$2fwaffles$2fCode$2fWeb$2fwaffleiron$2fweb$2fwp$2dcontent$2fthemes$2fbelgium$2fsrc$2fscripts$2fmod$2ejs; file_$2fUsers$2fwaffles$2fCode$2fWeb$2fwaffleiron$2fweb$2fwp$2dcontent$2fthemes$2fbelgium$2fsrc$2fscripts$2fmod$2ejs.deps = {}; file_$2fUsers$2fwaffles$2fCode$2fWeb$2fwaffleiron$2fweb$2fwp$2dcontent$2fthemes$2fbelgium$2fsrc$2fscripts$2fmod$2ejs.filename = "/Users/waffles/Code/Web/waffleiron/web/wp-content/themes/belgium/src/scripts/mod.js"; function file_$2fUsers$2fwaffles$2fCode$2fWeb$2fwaffleiron$2fweb$2fwp$2dcontent$2fthemes$2fbelgium$2fsrc$2fscripts$2fmod$2ejs(module, exports, require, __filename, __dirname, __import_meta) {
 ~function() {
 //
 // mod.js
 // declare 
 
-  (async ({
-    $,
-  }) => {
-    $(document).ready(function() {
+(async ({
+  $,
+}) => {
+  $(document).ready(function() {
 
-      let $subMenu = false
-      let $dropdown = false
+    let $subMenu = false
+    let $dropdown = false
 
-      $(window).hover(
-        ({ target }) => {
-            if ($(target).is('.sub-menu-enabled')) {
-              console.log( $(target).parent().next() )
+    $(document).hover(
+      ({ target }) => {
+          if ($(target).is('.sub-menu-enabled')) {
+            console.log( $(target).parent().next() )
 
-              $dropdown = $(target).parent().next()
-              $subMenu = $(target)
+            $dropdown = $(target).parent().next()
+            $subMenu = $(target)
 
-              $subMenu.addClass('sub-menu-style')
-              $dropdown.slideDown('fast')
-              return
-            }
-          return
-        },
-        ({ target }) => {
-          console.log($subMenu)
-          console.log($dropdown)
-          if ($subMenu || $dropdown) {
-            if (!($(target).is('.dropdown-menu *')) && !($(target).is('.dropdown-menu')) && !($(target).is('.sub-menu-enabled'))) {
-              $('.sub-menu-enabled').removeClass('sub-menu-style')
-              $('.dropdown-menu').slideUp('fast')
-              return
-            }
-
-          return
+            $subMenu.addClass('sub-menu-style')
+            $dropdown.slideDown('fast')
           }
+      },
+      ({ target }) => {
+        // console.log($subMenu)
+        // console.log($dropdown)
+        if ($subMenu || $dropdown) {
+          if (!($(target).is('.dropdown-menu *')) && !($(target).is('.dropdown-menu')) && !($(target).is('.sub-menu-enabled'))) {
+            $('.sub-menu-enabled').removeClass('sub-menu-style')
+            $('.dropdown-menu').slideUp('fast')
+          }
+
         }
-      )
-
-//       // code
-//       $('.primary-menu .menu-link').mouseover(
-//         function() {
-//           subMenu = $(this).find('.sub-menu-enabled')
-//           dropdown = subMenu.parent().next('.dropdown-menu')
-
-//           subMenu.toggleClass('sub-menu-style')
-//           dropdown.slideDown('fast')
-
-//           // console.log(`mouseover: `, subMenu)
-//           // console.log(`mouseover: `, dropdown)
-//         }
-//       )
-
-//       $('.dropdown-menu').hover(
-//         function() {
-//           // let subMenu = $(this).prev().find('.sub-menu-enabled')
-//           // let dropdown = $(this)
-
-//           dropdown.css({display: 'flex !important'})
-//           dropdown.toggleClass('vis')
-//           subMenu.addClass('sub-menu-style')
-
-//           // console.log(`dropdown - mouseover: `, subMenu)
-//           // console.log(`dropdown - mouseover: `, dropdown)
-//         },
-//         function() {
-//           // let subMenu = $(this).prev().find('.sub-menu-enabled')
-//           // let dropdown = $(this)
-
-//           // dropdown.css('vis')
-//           dropdown.css({display: ''})
-//           subMenu.removeClass('sub-menu-style')
-//           dropdown.toggleClass('vis')
-//           // dropdown.slideUp('fast')
-
-//           // console.log(`dropdown - mouseout: `, subMenu , '\n')
-//           // console.log(`dropdown - mouseout: `, dropdown , '\n')
-//         }
-//       )
-
-//       $('.primary-menu .menu-link').mouseout(
-//         function() {
-//           // let subMenu = $(this).find('.sub-menu-enabled')
-//           // let dropdown = subMenu.parent().next('.dropdown-menu')
-
-//           subMenu.removeClass('sub-menu-style')
-
-//           // console.log(`mouseout: `, subMenu , '\n')
-//           // console.log(`mouseout: `, dropdown , '\n')
-
-//           if ( !(dropdown.hasClass('vis')) ) {
-//             dropdown.slideUp('fast')
-//           }
-//         }
-//       )
-
-      $('.carrousel').slick({
-        dots: true,
-        arrows: false,
-      })
-
-      $('.accordion-item').on('click', function() {
-        $(this).find('.accordion-content').slideToggle('fast')
-      })
+      }
+    )
+    $('.carrousel').slick({
+      dots: true,
+      arrows: false,
     })
 
-  })({
-    // function defs
-    $               : jQuery,
-    // slctr           : s => document.querySelector(`${ s }`),
-    // toggleBodyClass : c => document.body.classList.toggle(`${ c }`),
-    // addBodyClass    : c => document.body.classList.add(`${ c }`),
-    // removeBodyClass : c => document.body.classList.remove(`${ c }`),
+    $('.accordion-item').on('click', function() {
+      $(this).find('.accordion-content').slideToggle('fast')
+    })
   })
 
-
-      // $('.primary-menu .menu-link').mouseout(
-      //   function() {
-      //     let subMenu = $(this).find('.sub-menu-enabled')
-      //     let dropdown = $(this).find('.sub-menu-enabled').parent().next('.dropdown-menu')
-      //     if ($(this).find('.sub-menu-enabled')) {
-      //       dropdown.slideDown('fast')
-      //     }
-      //   }
-      // )
-
-      // $('.primary-menu .dropdown-menu').hover(
-      //   function(e) {
-      //     // $(this).parent().addClass('sub-menu-style')
-      //     $(this).parent().prev('.sub-menu-enabled').toggleClass('sub-menu-style')
-      //   },
-      //   function(e) {
-      //     $(this).parent().prev('.sub-menu-enabled').toggleClass('sub-menu-style')
-      //     $(this).slideToggle()
-      //     // $(this).parent().next('.dropdown-menu').slideToggle('fast')
-      //   }
-      // )
-
-      // $('.primary-menu .sub-menu-enabled').hover(
-      //   function() {},
-      //   function() {}
-      // )
+})({
+  // function defs
+  $               : jQuery,
+  // slctr           : s => document.querySelector(`${ s }`),
+  // toggleBodyClass : c => document.body.classList.toggle(`${ c }`),
+  // addBodyClass    : c => document.body.classList.add(`${ c }`),
+  // removeBodyClass : c => document.body.classList.remove(`${ c }`),
+})
 }()}
-  Pax.main = file_$2fUsers$2fwaffles$2f$2edotfiles$2fCode$2fWeb$2fwaffleiron$2fweb$2fwp$2dcontent$2fthemes$2fbelgium$2fsrc$2fscripts$2fmod$2ejs; Pax.makeRequire(null)()
+  Pax.main = file_$2fUsers$2fwaffles$2fCode$2fWeb$2fwaffleiron$2fweb$2fwp$2dcontent$2fthemes$2fbelgium$2fsrc$2fscripts$2fmod$2ejs; Pax.makeRequire(null)()
   if (typeof module !== 'undefined') module.exports = Pax.main.module && Pax.main.module.exports
 }(typeof global !== "undefined" ? global : typeof window !== "undefined" ? window : this)
 //# sourceMappingURL=out.js.map
--- web/wp-content/themes/belgium/src/views/designs/contact.blade.php
+++ web/wp-content/themes/belgium/src/views/designs/contact.blade.php
@@ -1,5 +1,8 @@
 <section class="contact w-full relative container">
   <div class="container-sm w-full h-full">
-  {!! do_shortcode('[advanced_form form="form_5d49e4b780c0e"]') !!}
+    @php
+      {{-- echo advanced_form("form_5d49e4b780c0e"); --}}
+    @endphp
+  {{-- {!! advanced_form("form_5d49e4b780c0e") !!} --}}
   </div>
 </section>
