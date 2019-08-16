8ba08af11c5af8801ad5116bf570dab14f12ce82
cwahlfeldt <chriswahlfeldt@gmail.com>
2019-08-16 16:33:08 -0500

    tight

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit3a4da2d305652ee320a9dd4644605630::getLoader();
+return ComposerAutoloaderInite6515e84ac7b7e0e8536a245d1fac782::getLoader();
--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -7,10 +7,4 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'johnpbloch\\Composer\\' => array($vendorDir . '/johnpbloch/wordpress-core-installer/src'),
-    'WebDriver' => array($vendorDir . '/instaclick/php-webdriver/lib'),
-    'SensioLabs\\Behat\\PageObjectExtension\\' => array($vendorDir . '/sensiolabs/behat-page-object-extension/src'),
-    'ProxyManager\\' => array($vendorDir . '/ocramius/proxy-manager/src'),
-    'Mockery' => array($vendorDir . '/mockery/mockery/library'),
-    'Behat\\MinkExtension' => array($vendorDir . '/behat/mink-extension/src'),
-    'Behat\\Gherkin' => array($vendorDir . '/behat/gherkin/src'),
 );
