f6e86bf82f7b5587725728801418a12655797576
cwahlfeldt <chriswahlfeldt@gmail.com>
2019-08-28 13:04:27 -0500

    cool

---
+++ web/sites/default/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * If there is a local settings file, then include it
+ */
+$local_settings = __DIR__ . "/settings.local.php";
+if (file_exists($local_settings)) {
+  include $local_settings;
+}
